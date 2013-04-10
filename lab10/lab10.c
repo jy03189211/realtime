f1615f219c20a67f8d7c2d68ca905db9b6a11c57
Silence <jinjin@server.local>
2013-04-11 01:46:54 +0300

    lab10 done
    2 questions to be answered

--- lab10/lab10.c
+++ lab10/lab10.c
@@ -3,35 +3,42 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <sys/select.h>
-#define MAXLINE 80
-
+#include <sys/time.h>
+#include <sys/types.h>
 int OpenChatFellow();
-void counter(int i);
 int main(int argc, const char * argv[])
 {
     char chr_fellow,chr_own;
-    int fellow_desc,fd_kb,n,r;
-    fd_set fdset;
-
-    FD_ZERO(&fdset);
-    FD_SET(fd_kb, &fdset);
-    FD_SET(fellow_desc, &fdset);
-    
+    int fellow_desc,n,r;
+    fd_set rd_set,wrt_set;
     fellow_desc=OpenChatFellow();
-    n=select(fellow_desc+1,&fdset,NULL,NULL,NULL);
-    //parent
-    while((r=read(fellow_desc, &chr_fellow, 1))>0||chr_own!='q'&&chr_own!='Q'){
-        if(n>0){
-            if (FD_ISSET(fd_kb,&fdset))
-            {
-                read(0,&chr_own,1);
-                write(1,&chr_own,1);
-            }
-            if (FD_ISSET(fellow_desc,&fdset))
-            {
+    
+    do{
+            FD_ZERO(&rd_set);
+            //FD_ZERO(&wrt_set);
+            //FD_SET(1, &wrt_set);
+            FD_SET(0, &rd_set);
+            FD_SET(fellow_desc, &rd_set);
+            //(1)if  FD_SET(1, &wrt_set);
+            //and set &wrt_set as the 3rd parameter of select()
+            //why it doesn't work???????????????????????????????????
+            n=select(fellow_desc+1,&rd_set,NULL,NULL,NULL);
+            //(2)printf("%d!!!\n",n); why it always prints one ????????
+
+            //read kb
+            if (FD_ISSET(fellow_desc,&rd_set))
+            {   
+                r=read(fellow_desc,&chr_fellow,1);
                 write(1,&chr_fellow,1);
             }
-        }    
-    }
+           //read fellow 
+            if((chr_own!='q'&&chr_own!='Q')){
+                if (FD_ISSET(0,&rd_set))
+                {   
+                    read(0,&chr_own,1);
+                    write(1,&chr_own,1);
+                }
+            }
+        }while(r>0);
     return 0;
 }
