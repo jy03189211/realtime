aea72eac4b3418a236cf8703f9fd250e45a476fb
Silence <jinjin@server.local>
2013-04-26 05:06:04 +0300

    lab13done

--- lab13/phase2/lab13.c
+++ lab13/phase2/lab13.c
@@ -14,37 +14,37 @@ static pthread_mutex_t mtx=PTHREAD_MUTEX_INITIALIZER;
 pthread_t t[11];
 
 static void *threadFunc(void *arg){
-	int loops=*((int *)arg);
-	int loc,s;
-	long i;
-	for (i = 0; i < loops; i++)
-	{
-		 s = pthread_mutex_lock(&mtx);
-		 loc=counter;
-		 loc++;
-		 counter=loc;
-		 s=pthread_mutex_unlock(&mtx);
-	}
+    int loops=*((int *)arg);
+    int loc,s;
+    long i;
+    for (i = 0; i < loops; i++)
+    {
+         s = pthread_mutex_lock(&mtx);
+         loc=counter;
+         loc++;
+         counter=loc;
+         s=pthread_mutex_unlock(&mtx);
+    }
 
-	return (void *)loc;
+    return (void *)i;
 
 }
 
 int main(int argc, const char * argv[])
-{	
-	int i,loops=100000;
-	void *ret;
-	for (i = 0; i < 10; i++)
-	{	
-		pthread_create(&t[i],NULL,threadFunc,&loops);
-	}
-	for (	i = 0; i < 10; ++i)
-	{
-		pthread_join(t[i], &ret);
-		printf("thread %d :%ld\n",i,(long)ret);
-	}
-	printf("glob counter:%d\n",counter);
-	printf("lost increments:%d\n",loops*10-counter);
-	return 0;
+{
+    int i,loops=100000;
+    void *ret;
+    for (i = 0; i < 10; i++)
+    {
+        pthread_create(&t[i],NULL,threadFunc,&loops);
+    }
+    for (   i = 0; i < 10; ++i)
+    {
+        pthread_join(t[i], &ret);
+        printf("thread %d :%ld\n",i,(long)ret);
+    }
+    printf("glob counter:%d\n",counter);
+    printf("lost increments:%d\n",loops*10-counter);
+    return 0;
 }
 
